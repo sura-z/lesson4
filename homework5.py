80f6b4575f12f7b61825933e520d1ba87666e106
sura-z <169043346+sura-z@users.noreply.github.com>
2024-05-12 12:26:57 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,11 @@
+my_list = ['cocos', 'apple', 'orange', 'grey', 'banana']
+print(my_list)
+print(my_list [0])
+print(my_list [-1])
+print(my_list [2: 4])
+my_list[2] = "peash"
+print(my_list)
+my_dict = {'cocos': 'кокос', 'apple': 'яблоко', 'orange': 'апельсин', 'grey': 'грейпфрут'}
+print(my_dict)
+my_dict['cocos'] = 'яблоко'
+print(my_dict)
